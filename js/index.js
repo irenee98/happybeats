4391aa7bd96c7f0627e1231ea645587a99af9d5b
irenee98 <irenerogas@gmail.com>
2025-10-03 18:08:04 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,29 +1,42 @@
 function cargar() {
-    var objHttp = new XMLHttpRequest();
-    var url = "https://api.allorigins.win/raw?url=" + encodeURIComponent("https://noticias-music0.webnode.es/rss/all.xml");
+    const feedUrl = "https://noticias-music0.webnode.es/rss/all.xml";
+    const proxys = [
+        "https://api.allorigins.win/raw?url=",
+        "https://api.codetabs.com/v1/proxy?quest=",
+        "https://corsproxy.io/?"
+    ];
 
-    objHttp.open("GET", url, true);
-
-    objHttp.onreadystatechange = function () {
-        if (objHttp.readyState === 4 && objHttp.status === 200) {
-            var parser = new DOMParser();
-            var documento = parser.parseFromString(objHttp.responseText, "text/xml");
-            var items = documento.getElementsByTagName("item");
-            var cadena = "";
-
-            for (var i = 0; i < 3 && i < items.length; i++) {
-                var titulo = items[i].getElementsByTagName("title")[0].textContent;
-                var descripcion = items[i].getElementsByTagName("description")[0].textContent;
-                var enlace = items[i].getElementsByTagName("link")[0].textContent;
-
-                cadena += "Titular: <b>" + titulo + "</b><br>";
-                cadena += "Descripción: " + descripcion + "<br>";
-                cadena += "Enlace: <a href='" + enlace + "' target='_blank'>" + enlace + "</a><br><br>";
-            }
-
-            document.getElementById("caja").innerHTML = cadena;
+    // probamos cada proxy hasta que funcione uno
+    function intentarProxy(i) {
+        if (i >= proxys.length) {
+            document.getElementById("caja").innerHTML = "No se pudo cargar el feed (todos los proxys fallaron)";
+            return;
         }
-    };
 
-    objHttp.send();
+        fetch(proxys[i] + encodeURIComponent(feedUrl))
+            .then(res => {
+                if (!res.ok) throw new Error("Fallo en el proxy");
+                return res.text();
+            })
+            .then(str => {
+                const parser = new DOMParser();
+                const doc = parser.parseFromString(str, "text/xml");
+                const items = doc.getElementsByTagName("item");
+
+                let cadena = "";
+                for (let j = 0; j < 3 && j < items.length; j++) {
+                    const titulo = items[j].getElementsByTagName("title")[0].textContent;
+                    const descripcion = items[j].getElementsByTagName("description")[0].textContent;
+                    const enlace = items[j].getElementsByTagName("link")[0].textContent;
+
+                    cadena += `Titular: <b>${titulo}</b><br>`;
+                    cadena += `Descripción: ${descripcion}<br>`;
+                    cadena += `Enlace: <a href="${enlace}" target="_blank">${enlace}</a><br><br>`;
+                }
+                document.getElementById("caja").innerHTML = cadena;
+            })
+            .catch(() => intentarProxy(i + 1));
+    }
+
+    intentarProxy(0);
 }
