65a3ae25dbcbc397802c19223812ca6335d30e95
irenee98 <irenerogas@gmail.com>
2025-10-03 02:10:51 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,10 @@
 function cargar() {
     var objHttp = new XMLHttpRequest();
-    var url = "https://api.allorigins.win/raw?url=" + encodeURIComponent("https://noticias-music0.webnode.es/rss/all.xml");
-
-    objHttp.open("GET", url, true);
+    objHttp.open("GET", "https://noticias-music0.webnode.es/rss/all.xml", true);
 
     objHttp.onreadystatechange = function () {
         if (objHttp.readyState === 4 && objHttp.status === 200) {
-            var parser = new DOMParser();
-            var documento = parser.parseFromString(objHttp.responseText, "text/xml");
+            var documento = objHttp.responseXML;
             var items = documento.getElementsByTagName("item");
             var cadena = "";
 
@@ -27,3 +24,5 @@ function cargar() {
 
     objHttp.send();
 }
+
+
